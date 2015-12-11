ed631ebb2f3f3396e0e31098a28915b5a0462b66
MKS-students <mks.students@hackreactor.com>
2015-12-11 12:32:48 -0600

    working on service.js shorten

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,55 @@
 angular.module('shortly.services', [])
 
-.factory('Links', function ($http) {
-  // Your code here
-})
+// .factory('Links', function ($http, $location, Auth) {
+//   // Your code here
+//   if(Auth.isAuth) {
+
+//     var title = function(link) {
+//       return $http({
+
+//       })
+//     }
+
+
+//     // return {
+//     //   visits:     ,
+//     //   title:      ,
+//     //   original:   ,
+//     //   base_url:'http://localhost:8000',
+//     //   code:
+//     // }
+
+//   } else {
+//     $location.path('/signin');
+//   }
+
+// .factory('Shorten', function ($http, $location, Auth) {
+//   if(Auth.isAuth) {
+
+//     var title = function(link) {
+//       return $http({
+//         method: 'POST',
+//         url: '/api/links/shorten'
+//         data
+//       })
+//     }
+
+
+//     return {
+//       visits:     ,
+//       title:      ,
+//       original:   ,
+//       url:'http://localhost:8000',
+//       code:
+//     }
+
+//   } else {
+//     $location.path('/signin');
+//   }
+
+// })
+
+// })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
